b2779429af483c618aef16d610dd7afa46d0a4d0
Alson Shareef <alson.shareef@gmail.com>
2019-03-23 18:45:53 +0800

    Definition

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 // Trying to understand OOP.
 
+/*
+
+DEFINITION:
+Objected-Oriented Programming is an approach to programming, or a programming paradigm that revolves around organising your data and functionality through objects and their methods. This assigns certain types of information to its own singular object; keeping any relevant information in one place which removes clashing code and keeps things organized. Any kind of functionality that is associated to a particular type of information will be stored in its relevant object as a 'method'.
+
+*/
